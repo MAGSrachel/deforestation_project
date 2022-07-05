faa996369e0aeeae03769a7bf246b68cc0937540
MAGSrachel <88173471+MAGSrachel@users.noreply.github.com>
2022-07-05 14:43:17 +1200

    Delete play_again.py

--- play_again.py
+++
@@ -1,9 +0,0 @@
-
-yes_no = replay = input("Do you wish to play the quiz again?")
-if replay == "yes" or replay == "y":
-    print("Welcome back to the quiz")
-elif replay == "no" or replay == "n":
-    print("------------------------------")
-    print("Thank you for playing the quiz")
-    print("------------------------------")
-    exit(0)
